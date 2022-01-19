0726d40d04e4476d6f6e8f81689bd7e3c17e3b67
AJT8112 <alexnderJT@hotmail.com>
2022-01-19 19:59:23 +0000

    HTML and CSS complete

---
+++ Styles/index.js
@@ -0,0 +1,9 @@
+//Validate information is a string.
+
+//Validate that the input is an email address by having @ in the middle.
+
+//IF yes, produce message by pulling the information from the rest of the form.
+
+//IF No, return message, email address not valid.
+
+//Execute function when button is clicked.
