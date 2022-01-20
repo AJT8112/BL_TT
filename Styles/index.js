25d9aa9f726356b9a08c232a4e1d83d201933fb7
AJT8112 <alexnderJT@hotmail.com>
2022-01-20 00:26:58 +0000

    Addition of JS to full text field to console log
    Next steps;
    - Extend to global scope.
    - Create input to thank you text in alert.
    - Validate email address input.

--- Styles/index.js
+++ Styles/index.js
@@ -1,4 +1,46 @@
-//Validate information is a string.
+//Set values of person OBJECT
+
+let valN = ""
+let valS = ""
+let valF = ""
+let valR = ""
+let valE = ""
+
+//create OBJECT to hold person data
+
+let person = {
+    firstName: valN,
+    surname: valS,
+    fatherName: valF,
+    relation: valR,
+    emailAdd: valE
+};
+
+//change VALUE of inputs to equal the text displayed in the text fields.
+
+function getNval(){
+    var valN = document.getElementById("N").value;
+    console.log(valN);
+}
+function getSval(){
+    var valS = document.getElementById("S").value;
+    console.log(valS);
+}
+function getFval(){
+    var valF = document.getElementById("F").value;
+    console.log(valF);
+}
+function getRval(){
+    var valR = document.getElementById("R").value;
+    console.log(valR);
+}
+function getEval(){s
+    var valE = document.getElementById("E").value;
+    console.log(valE);
+}
+
+
+//create person OBJECT to hold data
 
 //Validate that the input is an email address by having @ in the middle.
 
